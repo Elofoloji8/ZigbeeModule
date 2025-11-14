48fef0f8687b71d8cf6d4422f6250028f8e65814
Elif Buse Öztürk <elofoloji8@gmail.com>
2025-11-14 04:15:07 +0300

    Create sender.ino

---
+++ sender.ino
@@ -0,0 +1,32 @@
+#include <SoftwareSerial.h>
+
+#define trigPin 8
+#define echoPin 7 SoftwareSerial zigbee(2, 3); // RX, TX
+(10 = RX from ZigBee, 15 = TX to ZigBee)
+
+void setup() { Serial.begin(9600); // PC'ye bilgi için
+zigbee.begin(9600); // ZigBee ile haberleşme
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+delay(100);
+}
+
+void loop() {
+long duration; float distanceCm;
+
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+duration = pulseIn(echoPin, HIGH, 30000); // timeout 30 ms if (duration == 0) {
+distanceCm = -1; // no echo } else { distanceCm = (duration / 2.0) * 0.0343;
+}
+
+// Gönderilecek string
+String out = "MESAFE:" + String(distanceCm, 2) + "cm"; zigbee.println(out); // ZigBee'ye gönder
+Serial.println("Gonderildi -> " + out); // PC'ye log
+
+delay(500);
+}
